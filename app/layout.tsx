e5de3a18b92838ec16691c1233df6688b0a4d361
thinh <thinh.phung@apacpro.com>
2024-04-17 16:26:21 +0700

    update name

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
+  title: "TON Animals",
+  description: "TON Animals - Where your assets grow",
 };
 
 export default function RootLayout({
