9ddd96fd0f3b479ebee735bade397ef688d6d057
Tim <lonelyblue89@gmail.com>
2024-04-20 13:17:42 +0700

    init landing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,30 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "400"
+});
 
 export const metadata: Metadata = {
-  title: "TON Animals",
-  description: "TON Animals - Where your assets grow",
+  title: "TON Animal",
+  description: "TON Animal - Where your assets grow",
 };
 
 export default function RootLayout({
   children,
-}: {
+} : {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+        className={`${poppins.className} bg-[#0098eb] overflow-y-scroll overflow-x-hidden`}
       >
         <StarsCanvas />
         <Navbar />
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -19,35 +19,33 @@ const HeroContent = () => {
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-purple-700">TON ANIMAl</span>
+            <span className="text-[40px]">
+              {" "}
+              {" "}
+            </span>
+           </span>
+        </motion.div>
+
+        <motion.div
           variants={slideInFromTop}
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+            The next TON meme coin
           </h1>
-        </motion.div>
-
-        <motion.div
-          variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
-        >
-          <span>
-            Providing
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              {" "}
-              the best{" "}
-            </span>
-            project exprience
-          </span>
         </motion.div>
 
         <motion.p
           variants={slideInFromLeft(0.8)}
-          className="text-lg text-gray-400 my-5 max-w-[600px]"
+          className="text-lg text-white my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          Ton Animal aims to create opportunities for more users to experience the TON ecosystem before large-scale liquidity is brought to the TON ecosystem.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
